556302b75fedc597bde445eac80ae8b0e9f68054
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-11-08 18:04:39 +0400

    CaesarCipher.java

--- src/main/java/az/rock/lesson/cryptography/symmetric/caesar/Main.java
+++ src/main/java/az/rock/lesson/cryptography/symmetric/caesar/Main.java
@@ -3,10 +3,10 @@ package az.rock.lesson.cryptography.symmetric.caesar;
 public class Main {
     public static void main(String[] args) {
         CaesarCipher caesarCipher = new CaesarCipher();
-        String cipherText = caesarCipher.encrypt("This is a plain text",5);
+        String cipherText = caesarCipher.encrypt("XThis is a plain text",3);
         System.out.println(cipherText);
 
-        String plainText = caesarCipher.decrypt("YMNXENXEFEUQFNSEYJBY",5);
+        String plainText = caesarCipher.decrypt(cipherText,3);
         System.out.println(plainText);
     }
 }
